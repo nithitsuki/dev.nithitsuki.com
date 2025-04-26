3785cd7b62cf8fdffea06c934431fe64d69ec739
nithitsuki <nithilanr@gmail.com>
2025-04-26 11:56:31 +0530

    added sum particles

---
+++ src/components/ParticlesBackground.tsx
@@ -0,0 +1,35 @@
+// components/ParticlesBackground.tsx
+"use client";
+
+import { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { type Container, type ISourceOptions } from "@tsparticles/engine";
+import { loadAll } from "@tsparticles/all";
+import particleConfig from "./particlesjs-config.json";
+
+export default function ParticlesBackground() {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadAll(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
+
+  return (
+    init && (
+      <Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={particleConfig as ISourceOptions}
+        className="absolute inset-0 -z-10"
+      />
+    )
+  );
+}
